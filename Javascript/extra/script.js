901bd1f9009e6128e64fac7d2683caf2560ca007
Joeyvdkuijl <55228974+Joeyvdkuijl@users.noreply.github.com>
2020-04-03 19:26:12 +0200

    files push

---
+++ Javascript/extra/script.js
@@ -0,0 +1,29 @@
+
+// let getalx = Math.floor(Math.random() * 11);
+
+// function tafel (getal){
+//     for(i=0; i < 10; i++) {
+//         let element = i * getal;
+//         console.log (element);
+//     }
+// }
+
+// tafel(4);
+
+// function barry(tafel) {
+//     let achmed = Math.floor(Math.random() * 11);
+//     console.log (achmed * 2);
+// }
+
+// barry();
+
+let rando = Math.floor(Math.random() * 100);
+
+function anders (flo){
+    for(i = 1; i < 10; i++){
+    let que = i * flo;
+    console.log (que);
+    }
+}
+
+anders(rando);
